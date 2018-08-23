1b9a2a67e8ba8efa213cebcc312575ee9feb3969
Reona Oshima <totoraj930@yahoo.co.jp>
2018-08-24 07:38:03 +0900

    minSdkVersionを21に戻した&コードの修正&読み込むスクリプトをAndroid用に変更

--- app/src/main/java/net/totoraj/tjdeck/MainActivity.java
+++ app/src/main/java/net/totoraj/tjdeck/MainActivity.java
@@ -2,18 +2,13 @@ package net.totoraj.tjdeck;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.MutableContextWrapper;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
@@ -32,15 +27,9 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
     private String TAG = "TJDeck";
-    private View mContent;
     private WebView mWebView = null;
-    private String tjDeckScript = "";
-    private String tjCheckScript = "";
-    private String regexDeck = "^https://tweetdeck\\.twitter\\.com.*$";
-    private String regexTwitter = "^https://(.+\\.|)twitter\\.com/(login|logout|sessions|account/login_verification).*$";
 
     public static final int INPUT_FILE_REQUEST_CODE = 1;
-    public static final String EXTRA_FROM_NOTIFICATION = "EXTRA_FORM_NOTIFICATION";
     private ValueCallback<Uri[]> mFilePathCallback;
     private String mCameraPhotoPath;
 
@@ -49,22 +38,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-
-
-        /* jsの用意 */
-        try {
-            if (BuildConfig.DEBUG) {
-                Log.d(TAG, "デバッグ用");
-                tjDeckScript = loadAssets("tj-deck-debug.js");
-            } else {
-                Log.d(TAG, "リリース用");
-                tjDeckScript = loadAssets("tj-deck.js");
-            }
-            tjCheckScript = loadAssets("test.js");
-        } catch (Exception error) {
-            Log.d(TAG, "onError");
-        }
 
         mWebView = (WebView) findViewById(R.id.webView1);
 
@@ -86,36 +59,17 @@ public class MainActivity extends Activity {
         if (savedInstanceState == null) {
             mWebView.loadUrl("https://tweetdeck.twitter.com");
         }
-
-
-
-    }
-
-    /* アセットからjsファイルを読み込んでStringで返すやつ */
-    private String loadAssets(String fileName) throws Exception {
-        StringBuilder res = new StringBuilder();
-        InputStream is = null;
-        BufferedReader br = null;
-        is = getAssets().open(fileName);
-        br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-
-        String tmp;
-        while ( (tmp = br.readLine()) != null) {
-            res.append(tmp);
-        }
-
-        return res.substring(0);
     }
 
 
     /* 戻るボタンでブラウザバックするようにする */
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
-            return true;
+            return;
         }
-        return super.onKeyDown(keyCode, event);
+        super.onBackPressed();
     }
 
     /* WebViewの内容を保持する */
@@ -125,13 +79,14 @@ public class MainActivity extends Activity {
         mWebView.saveState(outState);
     }
 
-    /* WebViewの内容を保持する */
+    /* WebViewの内容を戻す */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         mWebView.restoreState(savedInstanceState);
     }
 
+    /* Activityから値を受け取る */
     @Override
     public void onActivityResult (int requestCode, int resultCode, Intent data) {
         if(requestCode != INPUT_FILE_REQUEST_CODE || mFilePathCallback == null) {
@@ -141,10 +96,10 @@ public class MainActivity extends Activity {
 
         Uri[] results = null;
 
-        // Check that the response is a good one
+        // 正常な返り値か
         if(resultCode == Activity.RESULT_OK) {
             if(data == null) {
-                // If there is not data, then we may have taken a photo
+                // データがない場合は写真を撮った可能性があります？？？
                 if(mCameraPhotoPath != null) {
                     results = new Uri[]{Uri.parse(mCameraPhotoPath)};
                 }
@@ -162,71 +117,29 @@ public class MainActivity extends Activity {
     }
 
 
-    // https://github.com/googlearchive/chromium-webview-samples/blob/master/input-file-example/app/src/main/java/inputfilesample/android/chrome/google/com/inputfilesample/MainFragment.java
-    public class TJChromeClient extends WebChromeClient {
-        public boolean onShowFileChooser(
-                WebView webView, ValueCallback<Uri[]> filePathCallback,
-                WebChromeClient.FileChooserParams fileChooserParams) {
-            if (mFilePathCallback != null) {
-                mFilePathCallback.onReceiveValue(null);
-            }
-            mFilePathCallback = filePathCallback;
-
-            Intent takePicutureIntent = new Intent(MediaStore.INTENT_ACTION_MEDIA_SEARCH);
-            if (takePicutureIntent.resolveActivity(getPackageManager()) != null) {
-                File photoFile = null;
-                try {
-                    photoFile = createImageFile();
-                    takePicutureIntent.putExtra("PhotoPath", mCameraPhotoPath);
-                } catch (IOException error) {
-
-                }
-
-                if (photoFile != null) {
-                    mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
-                    takePicutureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                } else {
-                    takePicutureIntent = null;
-                }
-            }
-
-            Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
-            contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
-            contentSelectionIntent.setType("*/*");
-
-            Intent[] intentArray;
-            if (takePicutureIntent != null) {
-                intentArray = new Intent[]{takePicutureIntent};
-            } else {
-                intentArray = new Intent[0];
-            }
-
-            Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
-            chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
-            chooserIntent.putExtra(Intent.EXTRA_TITLE, "Media Chooser");
-            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
-            chooserIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-            String[] mimetypes = {"image/*", "video/*"};
-            chooserIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
-
-            startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
-
-            return true;
-        }
-
-        private File createImageFile() throws IOException {
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            String fileName = timeStamp;
-            File storageDir = Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOCUMENTS);
-            File file = File.createTempFile(fileName, "*", storageDir);
-            return file;
-        }
-    }
-
-
     /* WebViewのクライアント */
     public class TJClient extends WebViewClient {
+        private String tjDeckScript = "";
+        private String tjCheckScript = "";
+        private String regexDeck = "^https://tweetdeck\\.twitter\\.com.*$";
+        private String regexTwitter = "^https://(.+\\.|)twitter\\.com/(login|logout|sessions|account/login_verification).*$";
+
+        public TJClient() {
+            /* jsの用意 */
+            try {
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "デバッグ用");
+                    tjDeckScript = loadAssets("tj-deck-debug.js");
+                } else {
+                    Log.d(TAG, "リリース用");
+                    tjDeckScript = loadAssets("tj-deck.js");
+                }
+                tjCheckScript = loadAssets("test.js");
+            } catch (IOException error) {
+                Log.d(TAG, "onError");
+            }
+        }
+
         // ページのロード終了時
         @Override
         public void onPageFinished(WebView view, String url) {
@@ -253,15 +166,21 @@ public class MainActivity extends Activity {
             return super.shouldOverrideUrlLoading(view, request);
         }
 
+        /* アセットからjsファイルを読み込んでStringで返すやつ */
+        private String loadAssets(String fileName) throws IOException {
+            StringBuilder res = new StringBuilder();
+            InputStream is = null;
+            BufferedReader br = null;
+            is = getAssets().open(fileName);
+            br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
 
-//        @Override
-//        public void onLoadResource(WebView view, String url) {
-//            Log.d(TAG, "onLoadResource: " + url);
-//        }
-//        @Override
-//        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-//            handler.proceed();
-//        }
+            String tmp;
+            while ( (tmp = br.readLine()) != null) {
+                res.append(tmp);
+            }
+
+            return res.substring(0);
+        }
 
         /* tj-deck.jsを実行する */
         private void runTJDeckScript(final WebView view, final String url) {
@@ -296,5 +215,68 @@ public class MainActivity extends Activity {
             }
         }
 
+    }
+
+    // https://github.com/googlearchive/chromium-webview-samples/blob/master/input-file-example/app/src/main/java/inputfilesample/android/chrome/google/com/inputfilesample/MainFragment.java
+    public class TJChromeClient extends WebChromeClient {
+        public boolean onShowFileChooser(
+                WebView webView, ValueCallback<Uri[]> filePathCallback,
+                WebChromeClient.FileChooserParams fileChooserParams) {
+            if (mFilePathCallback != null) {
+                mFilePathCallback.onReceiveValue(null);
+            }
+            mFilePathCallback = filePathCallback;
+
+            Intent takeMediaIntent = new Intent(MediaStore.INTENT_ACTION_MEDIA_SEARCH);
+            if (takeMediaIntent.resolveActivity(getPackageManager()) != null) {
+                File mediaFile = null;
+                try {
+                    mediaFile = createMediaFile();
+                    takeMediaIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                } catch (IOException error) {
+                    Log.d(TAG, "onError");
+                }
+
+                if (mediaFile != null) {
+                    mCameraPhotoPath = "file:" + mediaFile.getAbsolutePath();
+                    takeMediaIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mediaFile));
+                } else {
+                    takeMediaIntent = null;
+                }
+            }
+
+            Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            contentSelectionIntent.setType("*/*");
+
+            Intent[] intentArray;
+            if (takeMediaIntent != null) {
+                intentArray = new Intent[]{takeMediaIntent};
+            } else {
+                intentArray = new Intent[0];
+            }
+
+            Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+            chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+            chooserIntent.putExtra(Intent.EXTRA_TITLE, "Media Chooser");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+            chooserIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            String[] mimeTypes = {"image/*", "video/*"};
+            chooserIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+
+            startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+
+            return true;
+        }
+
+        /* Fileを作成する */
+        private File createMediaFile() throws IOException {
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String fileName = timeStamp;
+            File storageDir = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOCUMENTS);
+            File file = File.createTempFile(fileName, "*", storageDir);
+            return file;
+        }
     }
 }
